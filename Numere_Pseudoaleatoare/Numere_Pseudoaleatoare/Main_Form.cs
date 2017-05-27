7dfce1e418a39d31443c590ce771a179113723ec
Rares Popa <Rares_Popa_2008@Yahoo.com>
2017-05-27 15:35:32 +0300

    * Test

---
+++ Numere_Pseudoaleatoare/Numere_Pseudoaleatoare/Main_Form.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numere_Pseudoaleatoare
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Form_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello Fgt");
+        }
+
+        private void OpenFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
