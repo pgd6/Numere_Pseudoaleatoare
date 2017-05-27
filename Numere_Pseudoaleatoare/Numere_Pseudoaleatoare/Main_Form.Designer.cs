a8655e5d314ac2c9ca649b373799eeda7783bd58
Rares Popa <Rares_Popa_2008@Yahoo.com>
2017-05-27 16:24:23 +0300

    * Colors

--- Numere_Pseudoaleatoare/Numere_Pseudoaleatoare/Main_Form.Designer.cs
+++ Numere_Pseudoaleatoare/Numere_Pseudoaleatoare/Main_Form.Designer.cs
@@ -29,28 +29,69 @@
         private void InitializeComponent()
         {
             this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btn_File = new System.Windows.Forms.Button();
+            this.tb_File = new System.Windows.Forms.TextBox();
+            this.btn_ClearFile = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // OpenFileDialog
             // 
-            this.OpenFileDialog.FileName = "OpenFileDialog";
             this.OpenFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog_FileOk);
+            // 
+            // btn_File
+            // 
+            this.btn_File.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_File.Location = new System.Drawing.Point(12, 344);
+            this.btn_File.Name = "btn_File";
+            this.btn_File.Size = new System.Drawing.Size(75, 23);
+            this.btn_File.TabIndex = 0;
+            this.btn_File.Text = "Select File";
+            this.btn_File.UseVisualStyleBackColor = true;
+            this.btn_File.Click += new System.EventHandler(this.btn_File_Click);
+            // 
+            // tb_File
+            // 
+            this.tb_File.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tb_File.Location = new System.Drawing.Point(93, 346);
+            this.tb_File.Name = "tb_File";
+            this.tb_File.ReadOnly = true;
+            this.tb_File.Size = new System.Drawing.Size(315, 20);
+            this.tb_File.TabIndex = 1;
+            this.tb_File.TextChanged += new System.EventHandler(this.tb_File_TextChanged);
+            // 
+            // btn_ClearFile
+            // 
+            this.btn_ClearFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_ClearFile.Location = new System.Drawing.Point(414, 344);
+            this.btn_ClearFile.Name = "btn_ClearFile";
+            this.btn_ClearFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_ClearFile.TabIndex = 2;
+            this.btn_ClearFile.Text = "Clear File";
+            this.btn_ClearFile.UseVisualStyleBackColor = true;
+            this.btn_ClearFile.Click += new System.EventHandler(this.btn_ClearFile_Click);
             // 
             // Main_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(895, 617);
+            this.ClientSize = new System.Drawing.Size(680, 379);
+            this.Controls.Add(this.btn_ClearFile);
+            this.Controls.Add(this.tb_File);
+            this.Controls.Add(this.btn_File);
             this.Name = "Main_Form";
             this.Text = "Main";
             this.Load += new System.EventHandler(this.Main_Form_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.Button btn_File;
+        private System.Windows.Forms.TextBox tb_File;
+        private System.Windows.Forms.Button btn_ClearFile;
     }
 }
 
--- Numere_Pseudoaleatoare/Numere_Pseudoaleatoare/Main_Form.cs
+++ Numere_Pseudoaleatoare/Numere_Pseudoaleatoare/Main_Form.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,12 +20,60 @@ namespace Numere_Pseudoaleatoare
 
         private void Main_Form_Load(object sender, EventArgs e)
         {
-            Console.WriteLine("Hello Fgt");
+            btn_ClearFile_Toggle(false);
+
         }
 
         private void OpenFileDialog_FileOk(object sender, CancelEventArgs e)
         {
 
+        }
+
+        private void btn_File_Click(object sender, EventArgs e)
+        {
+            // Set the File Picker to the current directory.
+            OpenFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+
+            // Attempts to show the file dialog.
+            if (OpenFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // If successful do things.
+                System.IO.StreamReader sr = new System.IO.StreamReader(OpenFileDialog.FileName);
+
+                tb_File.Text = OpenFileDialog.FileName;
+
+                btn_ClearFile_Toggle(true);
+
+                sr.Close();
+            }
+        }
+
+        private void tb_File_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ClearFile_Click(object sender, EventArgs e)
+        {
+            tb_File.Text = String.Empty;
+            OpenFileDialog = new OpenFileDialog();
+
+            btn_ClearFile_Toggle(false);
+        }
+
+        private void btn_ClearFile_Toggle(bool state)
+        {
+            btn_ClearFile.ForeColor = Color.White;
+
+            if (state == true)
+            {
+                btn_ClearFile.BackColor = Color.DarkGreen;
+            }
+            else
+            {
+                btn_ClearFile.BackColor = Color.LightPink;
+            }
+            btn_ClearFile.Enabled = state;
         }
     }
 }
